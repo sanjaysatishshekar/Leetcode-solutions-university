eb86d19cfa1a20f77fb47c95a74b4e13ebb146ea
Sanjay S S <sanjay.india96@gmail.com>
2023-12-22 12:52:01 -0600

    Time: 3 ms (72.19%) | Memory: 42.8 MB (57.04%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,4 +1,28 @@
 class Solution {
+    
+    public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            char c1 = s.charAt(i);
+            char c2 = s.charAt(j);
+            if (!Character.isLetterOrDigit(c1)) {
+                i++;
+            }
+            else if (!Character.isLetterOrDigit(c2)) {
+                j--;
+            }
+            else {
+                if (Character.toLowerCase(c1) != Character.toLowerCase(c2))
+                    return false;
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+    
+    /*
     public boolean isPalindrome(String s) {
         StringBuilder sb = new StringBuilder();
         for (char c: s.toCharArray()) {
@@ -15,4 +39,5 @@ class Solution {
         }
         return true;
     }
+    */
 }
