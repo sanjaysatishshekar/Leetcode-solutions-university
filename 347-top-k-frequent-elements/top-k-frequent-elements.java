19e38fe89108149efdfe217a64808aa8488ebbf5
Sanjay S S <sanjay.india96@gmail.com>
2023-12-22 10:10:13 -0600

    Time: 11 ms (88.71%) | Memory: 47.7 MB (41.51%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,4 +1,37 @@
 class Solution {
+
+    public int[] topKFrequent(int[] nums, int k) {
+        int[] result = new int[k];
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        List<Integer>[] buckets = new List[nums.length + 1];
+
+        for (int num: nums) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        for (int key: frequencyMap.keySet()) {
+            int frequency = frequencyMap.get(key);
+            if (buckets[frequency] == null) {
+                buckets[frequency] = new ArrayList<>();
+            }
+            buckets[frequency].add(key);
+        }
+        int j = 0;
+        for (int i = buckets.length - 1; i >= 0; i--) {
+            if (buckets[i] == null) 
+                continue;
+            for (int num : buckets[i]) {
+                result[j] = num;
+                j = j + 1;
+                if (j == k) {
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+
+    /* Using PriorityQueue
     public int[] topKFrequent(int[] nums, int k) {
         int[] result = new int[k];
         Map<Integer, Integer> table = new HashMap<>();
@@ -17,4 +50,5 @@ class Solution {
         }
         return result;
     }
+    */
 }
