3eda9c4a3062e8832dd21c579e691ae44860ab92
Sanjay S S <sanjay.india96@gmail.com>
2024-01-02 13:14:01 -0600

    Time: 1 ms (88.54%) | Memory: 44.9 MB (12.42%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -16,20 +16,36 @@
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        if (root == null) return result;
-        q.add(root);
-        while(!q.isEmpty()) {
-            int size = q.size();
-            List<Integer> level = new ArrayList<>();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q.poll();
-                if (node != null) level.add(node.val);
-                if (node.left != null) q.add(node.left);
-                if (node.right != null) q.add(node.right); 
-            }
-            result.add(level);
-        }
+        levelOrder(root, 0, result);
         return result;
     }
+
+    public void levelOrder(TreeNode root, int level, List<List<Integer>> result) {
+        if (root == null) return;
+        if (result.size() == level) {
+            List<Integer> l = new ArrayList<>();
+            result.add(l);
+        }
+        result.get(level).add(root.val);
+        levelOrder(root.left, level + 1, result);
+        levelOrder(root.right, level + 1, result);
+    }
+    // public List<List<Integer>> levelOrder(TreeNode root) {
+    //     List<List<Integer>> result = new ArrayList<>();
+    //     Queue<TreeNode> q = new LinkedList<>();
+    //     if (root == null) return result;
+    //     q.add(root);
+    //     while(!q.isEmpty()) {
+    //         int size = q.size();
+    //         List<Integer> level = new ArrayList<>();
+    //         for (int i = 0; i < size; i++) {
+    //             TreeNode node = q.poll();
+    //             if (node != null) level.add(node.val);
+    //             if (node.left != null) q.add(node.left);
+    //             if (node.right != null) q.add(node.right); 
+    //         }
+    //         result.add(level);
+    //     }
+    //     return result;
+    // }
 }
