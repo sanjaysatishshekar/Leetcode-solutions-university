a852567b0b58d77135c466c9212623ed94cb0d7b
Sanjay S S <sanjay.india96@gmail.com>
2024-01-22 11:28:21 -0600

    Add method using indexes

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -22,4 +22,23 @@ class Solution {
         }
         return result;
     }
+
+
+    public int lengthOfLongestSubstring(String s) {
+        int[] table = new int[256];
+        int l = 0;
+        int n = s.length();
+        int result = 0;
+        char[] text = s.toCharArray();
+        for (int r = 0; r < n; r++) {
+            int index = text[r];
+            table[index]++;
+            while (l <= r && table[index] > 1) {
+                table[text[l]]--;
+                l++;
+            }
+            result = Math.max(result, r - l + 1);
+        }
+        return result;
+    }
 }
