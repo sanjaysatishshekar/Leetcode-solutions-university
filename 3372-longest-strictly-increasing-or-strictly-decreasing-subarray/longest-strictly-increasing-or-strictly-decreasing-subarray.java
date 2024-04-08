db67786ad630e3bce6d4c49fa2357cb2ed23896a
Sanjay S S <sanjay.india96@gmail.com>
2024-04-07 21:49:34 -0500

    Time: 1 ms (100.00%) | Memory: 42.7 MB (100.00%) - LeetSync

---
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestMonotonicSubarray(int[] nums) {
+        int result = 1;
+        int inc = 1, dec = 1;
+        if (nums == null || nums.length == 0) return -1; 
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i-1]) {
+                inc++;
+                dec = 1;
+            }
+            else if (nums[i] < nums[i - 1]) {
+                dec++;
+                inc = 1;
+            }
+            else {
+                inc = 1;
+                dec = 1;
+            }
+            result = Math.max(result, Math.max(inc, dec));
+        }
+        return result; 
+    }
+}
