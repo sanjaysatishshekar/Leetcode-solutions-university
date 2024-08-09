613d79e0448ef94b76165c6d68de107f64a52aeb
Sanjay S S <sanjay.india96@gmail.com>
2024-08-09 18:41:58 -0500

    Time: 0 ms (100.00%) | Memory: 40.8 MB (5.04%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -2,7 +2,19 @@ class Solution {
     Map<Integer, Integer> memo = new HashMap<>();
 
     public int tribonacci(int n) {
-        return dp(n);
+        // return dp(n);
+        if (n <= 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 1;
+        int[] dp = new int[n + 1];
+        dp[0] = 0; dp[1] = 1; dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+        return dp[n];
     }
 
     private int dp(int n) {
