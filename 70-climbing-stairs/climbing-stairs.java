657d63253c53670e3b50773a4492dcc28967ad3b
Sanjay S S <sanjay.india96@gmail.com>
2024-08-09 14:57:05 -0500

    Time: 0 ms (100.00%) | Memory: 40.5 MB (23.41%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,18 +1,13 @@
 class Solution {
+    private HashMap<Integer, Integer> dp = new HashMap<>();
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            // Find all the way so use Math.max
-            dp[i] = Math.max(dp[i - 1] + dp[i - 2], dp[i-2] + 1);
-
+        if (n <= 2)
+            return n;
+        if (!dp.containsKey(n)) {
+            dp.put(n, climbStairs(n - 1) + climbStairs(n - 2));
         }
-            // The condition is dp[i - 1] + dp[i - 2], dp[i-2] + 1
-            // because from dp[i - 2] it costs 1 step to reach dp[i]
-            // dp[i - 1] is cost to reach dp[i - 1]
-            // dp[i - 2] is cost to reach dp[i - 2]
-        // System.out.println(Arrays.toString(dp));
-        return dp[n];   
+
+        return dp.get(n);
+        
     }
 }
