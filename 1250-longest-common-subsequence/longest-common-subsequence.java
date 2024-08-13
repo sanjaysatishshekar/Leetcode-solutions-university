caf9b8b822788ffc5a60a6278e4655d9963ecb50
Sanjay S S <sanjay.india96@gmail.com>
2024-08-13 17:38:00 -0500

    Time: 20 ms (74.06%) | Memory: 50.8 MB (55.76%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,25 +1,41 @@
 class Solution {
-    private int dp(int i, int j, String a, String b, int[][] memo) {
-        if (i < 0 || j < 0) return 0;
-        if (i == 0 && j == 0) return a.charAt(i) == b.charAt(j) ? 1 : 0;
-        if (memo[i][j] == -1)  {
-            if (a.charAt(i) == b.charAt(j))
-                memo[i][j] = dp(i - 1, j - 1, a, b, memo) + 1;
-            else 
-                memo[i][j] = Math.max(dp(i - 1, j, a, b, memo), dp(i, j - 1, a, b, memo));
-        }
-        return memo[i][j];
-    }
+    // private int dp(int i, int j, String a, String b, int[][] memo) {
+    //     if (i < 0 || j < 0) return 0;
+    //     if (i == 0 && j == 0) return a.charAt(i) == b.charAt(j) ? 1 : 0;
+    //     if (memo[i][j] == -1)  {
+    //         if (a.charAt(i) == b.charAt(j))
+    //             memo[i][j] = dp(i - 1, j - 1, a, b, memo) + 1;
+    //         else 
+    //             memo[i][j] = Math.max(dp(i - 1, j, a, b, memo), dp(i, j - 1, a, b, memo));
+    //     }
+    //     return memo[i][j];
+    // }
+
+    // public int longestCommonSubsequence(String text1, String text2) {
+    //     int m = text1.length();
+    //     int n = text2.length();
+    //     int[][] memo = new int[m][n];
+    //     for (int i = 0; i < m; i++) {
+    //         for (int j = 0; j < n; j++) {
+    //             memo[i][j] = -1;
+    //         }
+    //     }
+    //     return dp(m - 1, n - 1, text1, text2, memo);
+    // }
 
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length();
         int n = text2.length();
-        int[][] memo = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                memo[i][j] = -1;
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]); 
             }
         }
-        return dp(m - 1, n - 1, text1, text2, memo);
+        return dp[m][n];
     }
 }
