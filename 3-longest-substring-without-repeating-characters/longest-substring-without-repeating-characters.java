c75562128c00ddbe6e9035a1da7834828a0b2019
Sanjay S S <sanjay.india96@gmail.com>
2024-09-13 01:30:00 -0500

    Time: 2 ms (98.94%) | Memory: 43.9 MB (84.32%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,43 +1,17 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int left = 0;
-        int right = 0;
         int result = 0;
-        int[] table = new int[128];
+        int i = 0, j = 0;
         int n = s.length();
-        while (right < n) {
-            char c = s.charAt(right);
-            int i = table[c];
-
-            // check if it is not within the substring
-            if (i >= 1 && i >= left) {
-                left = i;
-            }
-
-            result = Math.max(result, right - left + 1);
-
-            // Add table[c] = right + 1 because it is the next location for left
-            table[c] = ++right;
-            
-        }
-        return result;
-    }
-
-
-    public int lengthOfLongestSubstring(String s) {
         int[] table = new int[256];
-        int l = 0;
-        int n = s.length();
-        int result = 0;
-        char[] text = s.toCharArray();
-        for (int r = 0; r < n; r++) {
-            int index = text[r];
-            table[index]++;
-            while (l <= r && table[index] > 1) {
-                table[text[l]]--;
-                l++;
+        while (i < n && j < n) {
+            table[s.charAt(j)]++;
+            while (i <= j && i < n && table[s.charAt(j)] > 1) {
+                table[s.charAt(i)]--;
+                i++;
             }
-            result = Math.max(result, r - l + 1);
+            result = Math.max(result, j - i + 1);
+            j++;
         }
         return result;
     }
