34dabd5badb415c156c3bb2bf8ec7d888fe70794
Sanjay S S <sanjay.india96@gmail.com>
2024-09-30 14:02:32 -0500

    Time: 11 ms (72.46%) | Memory: 57.7 MB (66.17%) - LeetSync

--- 1896-maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.java
+++ 1896-maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.java
@@ -1,42 +1,20 @@
 class Solution {
-
-    // private int dp(int i, int left, int[] nums, int[] mults, int[][] memo) {
-    //     if (i == mults.length)
-    //         return 0;
-
-    //     int mul = mults[i];
-    //     int right = nums.length - 1 - (i - left);
-
-    //     if (memo[i][left] == 0) {
-    //         memo[i][left] = Math.max(
-    //                 mul * nums[left] + dp(i + 1, left + 1, nums, mults, memo),
-    //                 mul * nums[right] + dp(i + 1, left, nums, mults, memo));
-    //     }
-    //     return memo[i][left];
-    // }
-
-    // public int maximumScore(int[] nums, int[] multipliers) {
-    //     int n = nums.length;
-    //     int m = multipliers.length;
-    //     int[][] memo = new int[m][m];
-    //     return dp(0, 0, nums, multipliers, memo);
-    // }
-
     public int maximumScore(int[] nums, int[] multipliers) {
-        int n = nums.length;
         int m = multipliers.length;
-        int[][] dp = new int[m + 1][m + 1];
+        int n = nums.length;
+        int[][] memo = new int[m][m];
+        return dp(0, 0, nums, multipliers, memo);
+    }
 
-        for (int i = m - 1; i >= 0; i--) {
-            for (int left = i; left >= 0; left--) {
-                int mul = multipliers[i];
-                int right = n - 1 - (i - left);
-                dp[i][left] = Math.max(
-                    mul * nums[left] + dp[i + 1][left + 1],
-                    mul * nums[right] + dp[i + 1][left]
-                );
-            }
+    private int dp(int i, int left, int[] nums, int[] mult, int[][] memo) {
+        if (i == mult.length) return 0;
+        int mul = mult[i];
+        int right = nums.length - (i - left) - 1;
+
+        if (memo[i][left] == 0) {
+            memo[i][left] = Math.max(mul * nums[left] + dp(i + 1, left + 1, nums, mult, memo),
+            mul * nums[right] + dp(i + 1, left, nums, mult, memo));
         }
-        return dp[0][0];
+        return memo[i][left];
     }
 }
