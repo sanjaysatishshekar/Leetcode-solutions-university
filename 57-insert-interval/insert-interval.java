21545936b98a84c79b44a65e1d6d869a4783c38c
Sanjay S S <sanjay.india96@gmail.com>
2025-04-06 09:29:13 -0500

    Time: 4 ms (11.78%) | Memory: 45.1 MB (40.50%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,29 +1,44 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
+        if (intervals.length == 0)
+            return new int[][] { newInterval };
+
+        int n = intervals.length;
+        int target = newInterval[0];
+        int left = 0, right = n - 1;
+
+        // Binary search to find the position to insert newInterval
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (intervals[mid][0] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        // Insert newInterval at the found position
         List<int[]> result = new ArrayList<>();
-        int[] temp = newInterval;
-        for (int[] i: intervals) {
-            // No overlap
-            // if ((i[0] < temp[0] && i[1] < temp[0]) || (temp[1] < i[0] && temp[1] < i[1])) {
-            if ((i[1] < temp[0]) || (temp[1] < i[0])) {
-                if (i[0] > temp[1]) {
-                    result.add(temp);
-                    temp = i;
-                }
-                else {
-                    result.add(i);
-                }
-            }
-            else {
-                temp[0] = Math.min(temp[0], i[0]);
-                temp[1] = Math.max(temp[1], i[1]);
+        for (int i = 0; i < left; i++) {
+            result.add(intervals[i]);
+        }
+        result.add(newInterval);
+        for (int i = left; i < n; i++) {
+            result.add(intervals[i]);
+        }
+
+        // Merge overlapping intervals
+        List<int[]> merged = new ArrayList<>();
+        for (int[] interval : result) {
+            // If res is empty or there is no overlap, add the interval to the result
+            if (merged.isEmpty() ||
+                    merged.get(merged.size() - 1)[1] < interval[0]) {
+                merged.add(interval);
+                // If there is an overlap, merge the intervals by updating the end of the last interval in res
+            } else {
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], interval[1]);
             }
         }
-        result.add(temp);
-        int[][] r = new int[result.size()][2];
-        for (int i = 0; i < result.size(); i++) {
-            r[i] = result.get(i);
-        }
-        return r;
+
+        return merged.toArray(new int[0][]);
     }
 }
