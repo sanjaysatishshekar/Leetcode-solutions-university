45a535ca11e24e6e6b6d48be320f677631ccf8a3
Sanjay S S <sanjay.india96@gmail.com>
2025-04-07 03:00:59 -0500

    Time: 0 ms (100.00%) | Memory: 44.7 MB (10.48%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -13,22 +13,41 @@
  *     }
  * }
  */
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         if (root == null) return true;
+//         List<Integer> tree = new ArrayList<>();
+//         inorder(root, tree);
+//         for (int i = 1; i < tree.size(); i++) {
+//             if (tree.get(i) <= tree.get(i - 1))
+//                 return false;
+//         }    
+//         return true;
+//     }
+
+//     private void inorder(TreeNode root, List<Integer> tree) {
+//         if (root == null) return;
+//         inorder(root.left, tree);
+//         tree.add(root.val);
+//         inorder(root.right, tree);
+//     }
+// }
+
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        if (root == null) return true;
-        List<Integer> tree = new ArrayList<>();
-        inorder(root, tree);
-        for (int i = 1; i < tree.size(); i++) {
-            if (tree.get(i) <= tree.get(i - 1))
-                return false;
-        }    
-        return true;
+        return isValidBST(root, null, null);
     }
 
-    private void inorder(TreeNode root, List<Integer> tree) {
-        if (root == null) return;
-        inorder(root.left, tree);
-        tree.add(root.val);
-        inorder(root.right, tree);
+    private boolean isValidBST(TreeNode node, Integer leftBound, Integer rightBound) {
+        if(node == null) {
+            return true;
+        }
+
+        if((leftBound == null || node.val > leftBound) &&
+            (rightBound == null || node.val < rightBound)) {
+            return isValidBST(node.left, leftBound, node.val) && isValidBST(node.right, node.val, rightBound);
+        }
+
+        return false;
     }
 }
