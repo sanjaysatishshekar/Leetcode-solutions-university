ccf0d241b45fc5a366da8f285810b6a12daf8cc3
Sanjay S S <sanjay.india96@gmail.com>
2025-04-07 20:15:17 -0500

    Time: 2 ms (94.91%) | Memory: 46.7 MB (49.66%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,40 +1,11 @@
 class Solution {
-    // public int[] countBits(int n) {
-    // int[] result = new int[n + 1];
-    // for (int i = 0; i <= n; i++) {
-    // int count = 0;
-    // for (int j = 0; j < 32; j++) {
-    // int bit = (i >> j) & 1;
-    // if (bit == 1) count++;
-    // }
-    // result[i] = count;
-    // }
-    // return result;
-    // }
-
-    // public int[] countBits(int n) {
-    //     int res[] = new int[n + 1];
-    //     for (int num = 0; num <= n; num++) {
-    //         res[num] = (bitCount(num));
-    //     }
-    //     return res;
-    // }
-
-    // private int bitCount(int num) {
-    //     int count = 0;
-    //     while (num > 0) {
-    //         count++;
-    //         num &= (num - 1);
-    //     }
-    //     return count;
-    // }
-
-    public int[] countBits(int num) {
-        int[] ans = new int[num + 1];
-        for (int x = 1; x <= num; ++x) {
-            ans[x] = ans[x & (x - 1)] + 1;
+    public int[] countBits(int n) {
+        int[] result = new int[n + 1];
+        int offset = 1;
+        for (int i = 1; i <= n; i++) {
+            if (offset * 2 == i) offset = i;
+            result[i] = 1 + result[i - offset];
         }
-        return ans;
+        return result;
     }
-
 }
