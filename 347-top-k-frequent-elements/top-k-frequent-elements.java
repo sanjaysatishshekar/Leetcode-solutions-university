d26f4b5b1d772bc69b3cede72a24ec8bdce286be
Sanjay S S <sanjay.india96@gmail.com>
2025-06-17 13:51:34 -0500

    Time: 13 ms (76.43%) | Memory: 49 MB (24.95%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,54 +1,18 @@
 class Solution {
-
     public int[] topKFrequent(int[] nums, int k) {
-        int[] result = new int[k];
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-        List<Integer>[] buckets = new List[nums.length + 1];
-
+        Map<Integer, Integer> counts = new HashMap<>();
         for (int num: nums) {
-            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+            counts.put(num, counts.getOrDefault(num, 0) + 1);
         }
-
-        for (int key: frequencyMap.keySet()) {
-            int frequency = frequencyMap.get(key);
-            if (buckets[frequency] == null) {
-                buckets[frequency] = new ArrayList<>();
-            }
-            buckets[frequency].add(key);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> counts.get(b) - counts.get(a));
+        for (int num: counts.keySet()) {
+            pq.offer(num);
         }
+        int[] result = new int[k];
         int j = 0;
-        for (int i = buckets.length - 1; i >= 0; i--) {
-            if (buckets[i] == null) 
-                continue;
-            for (int num : buckets[i]) {
-                result[j] = num;
-                j = j + 1;
-                if (j == k) {
-                    return result;
-                }
-            }
+        for (int i = k; i > 0; i--) {
+            result[j++] = pq.poll();
         }
         return result;
     }
-
-    /* Using PriorityQueue
-    public int[] topKFrequent(int[] nums, int k) {
-        int[] result = new int[k];
-        Map<Integer, Integer> table = new HashMap<>();
-        for (int num: nums) {
-            table.put(num, table.getOrDefault(num, 0) + 1);
-        }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> table.get(b) - table.get(a));
-        for (int key: table.keySet()) {
-            pq.add(key);
-        }
-        int i = 0;
-        for (int j = 0; j < k; j++) {
-            int val = pq.poll();
-            result[i] = val;
-            i++;
-        }
-        return result;
-    }
-    */
 }
