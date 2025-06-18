1dca9842e82a8a3146fd420b3a90a2b4272d907e
Sanjay S S <sanjay.india96@gmail.com>
2025-06-18 03:29:43 -0500

    Time: 0 ms (100.00%) | Memory: 41 MB (99.23%) - LeetSync

--- 1119-robot-bounded-in-circle/robot-bounded-in-circle.java
+++ 1119-robot-bounded-in-circle/robot-bounded-in-circle.java
@@ -1,19 +1,24 @@
 class Solution {
     public boolean isRobotBounded(String instructions) {
-        int[][] direction = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        int x = 0, y = 0;
-        int index = 0;
-        for (char c: instructions.toCharArray()) {
-            if (c == 'L')
-                index = (index + 3) % 4;
-            else if (c == 'R')
-                index = (index + 1) % 4;
-            else {
-                x += direction[index][0];
-                y += direction[index][1];
+        int[][] directions = new int[][] { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+        int d = 0;
+        int[] current = { 0, 0 };
+
+        for (char c : instructions.toCharArray()) {
+            if (c == 'G') {
+                current[0] += directions[d][0];
+                current[1] += directions[d][1];
+
+            } else if (c == 'R') {
+                d = (d + 1) % 4;
+            } else {
+                d = d - 1;
+                if (d == -1)
+                    d = 3;
             }
         }
-        return (x == 0 && y == 0) || index != 0;
-        
+        if (current[0] == 0 && current[1] == 0 || d != 0)
+            return true;
+        return false;
     }
 }
