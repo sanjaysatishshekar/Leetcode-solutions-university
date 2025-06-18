13051cbca0f52a0d9c5201ed49b9540eb347499a
Sanjay S S <sanjay.india96@gmail.com>
2025-06-17 19:44:49 -0500

    Time: 15 ms (77.93%) | Memory: 44.5 MB (58.63%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,23 +1,36 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
         int[] dp = new int[amount + 1];
-        Arrays.fill(dp, -1);
-        int result = coinCount(coins, amount, dp);
-        return result == Integer.MAX_VALUE ? -1 : result;
-    }
-
-    private int coinCount(int[] coins, int amount, int[] dp) {
-        if (amount < 0) return Integer.MAX_VALUE;
-        if (amount == 0) return 0;
-        if (dp[amount] != -1) return dp[amount];
-        int minCount = Integer.MAX_VALUE;
-        for (int coin: coins) {
-            int answer = coinCount(coins, amount - coin, dp);
-            if (answer != Integer.MAX_VALUE) {
-                minCount = Math.min(minCount, answer + 1);
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin: coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
             }
         }
-        dp[amount] = minCount;
-        return dp[amount];
+        return dp[amount] == max ? -1 : dp[amount];
     }
+    // public int coinChange(int[] coins, int amount) {
+    //     if (amount < 1) return 0;
+    //     return dp(coins, amount, new int[amount]);
+    // }
+
+    // private int dp(int[] coins, int rem, int[] count) {
+    //     if (rem < 0) return -1;
+    //     if (rem == 0) return 0;
+    //     if (count[rem - 1] != 0) return count[rem - 1];
+
+    //     int min = Integer.MAX_VALUE;
+    //     for (int coin: coins) {
+    //         int res = dp(coins, rem - coin, count);
+    //         if (res >= 0 && res < min) {
+    //             min = 1 + res;
+    //         }
+    //         count[rem - 1] = (min == Integer.MAX_VALUE) ? -1: min;
+    //     }
+    //     return count[rem - 1];
+    // }
 }
