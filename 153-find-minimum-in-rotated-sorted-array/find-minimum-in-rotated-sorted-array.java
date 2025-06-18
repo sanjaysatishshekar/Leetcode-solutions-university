af22fb8736819dad8c751c2b0ee4218fe6240ac2
Sanjay S S <sanjay.india96@gmail.com>
2025-06-17 20:57:41 -0500

    Time: 0 ms (100.00%) | Memory: 42.5 MB (13.08%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,37 +1,18 @@
 class Solution {
     public int findMin(int[] nums) {
-        int result = nums[0];
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            if (nums[left] <= nums[right])
-                result = Math.min(result, nums[left]);
-            int mid = left + (right - left) / 2;
-            result = Math.min(result, nums[mid]);
-            if (nums[mid] >= nums[left])
-                left = mid + 1;
-            else
-                right = mid - 1;                
+        int n = nums.length;
+        int low = 0, high = n - 1;
+        if (n == 1)
+            return nums[0];
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[low] <= nums[mid] && nums[mid] < nums[high])
+                return nums[low];
+            if (nums[mid] > nums[high])
+                low = mid + 1;
+            else 
+                high = mid;
         }
-        return result;
-    }
-    
-    public int findMin(int[] nums) {
-        if (nums.length == 1) return nums[0]; 
-        if (nums.length == 2) return Math.min(nums[0], nums[1]);
-        int l = 0;
-        int r = nums.length - 1;
-        while (l < r) {
-            int mid = (l + r) / 2;
-            if (nums[l] <= nums[mid] && nums[l] < nums[r]) {
-                return nums[l];
-            }
-            if (nums[mid] > nums[r]) {
-                l = mid + 1;
-            }
-            else {
-                r = mid;
-            }
-       } 
-       return nums[l];
+        return nums[low];       
     }
 }
