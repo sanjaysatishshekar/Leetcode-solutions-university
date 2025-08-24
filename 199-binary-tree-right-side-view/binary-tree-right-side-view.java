16be429f771953b16166a31ce44cb5dc4ad9707a
Sanjay S S <sanjay.india96@gmail.com>
2025-08-24 15:07:07 -0700

    Time: 0 ms (100.00%) | Memory: 42.4 MB (27.32%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -16,28 +16,16 @@
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        // dfs(root, result);
-        Queue<TreeNode> q = new LinkedList<>();
         if (root == null) return result;
-        q.offer(root);
-        while (!q.isEmpty()) {
-            int size = q.size();
-            List<Integer> level = new ArrayList<>();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q.poll();
-                level.add(node.val);
-                if (node.left != null) q.offer(node.left);
-                if (node.right != null) q.offer(node.right);
-            }
-            result.add(level.get(level.size() - 1));
-        }
+        result.add(root.val);
+        dfs(root, 0, result);
         return result;
     }
 
-    // private void dfs(TreeNode root, List<Integer> result) {
-    //     if (root == null) return;
-    //     result.add(root.val);
-    //     dfs(root.right, result);
-    //     dfs(root.left, result);
-    // }
+    private void dfs(TreeNode root, int depth, List<Integer> result) {
+        if (root == null) return;
+        if (depth == result.size()) result.add(root.val);
+        dfs(root.right, depth + 1, result);
+        dfs(root.left, depth + 1, result);
+    }
 }
