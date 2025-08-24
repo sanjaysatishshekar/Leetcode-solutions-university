ef4293ab72263e67fe2404a27881544cba5bd86c
Sanjay S S <sanjay.india96@gmail.com>
2025-08-24 13:45:49 -0700

    Time: 2 ms (28.81%) | Memory: 41.7 MB (40.36%) - LeetSync

---
+++ 394-decode-string/decode-string.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ']') {
+                List<Character> stringToAdd = new ArrayList<>();
+                while (stack.peek() != '[') {
+                    stringToAdd.add(stack.pop());
+                }
+                stack.pop();
+                int base = 1;
+                int k = 0;
+                while (!stack.isEmpty() && Character.isDigit(stack.peek())) {
+                    k = k + (stack.pop() - '0') * base;
+                    base = base * 10;
+                }
+
+                while (k != 0) {
+                    for (int j = stringToAdd.size() - 1; j >= 0; j--) {
+                        stack.push(stringToAdd.get(j));
+                    }
+                    k--;
+                }
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        while (!stack.isEmpty()) {
+            result.append(stack.pop());
+        }
+        return result.reverse().toString();
+    }
+}
