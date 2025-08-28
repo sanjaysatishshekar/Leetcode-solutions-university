d91ec802c33e5967b91baaf76ccd703d9f90b6b6
Sanjay S S <sanjay.india96@gmail.com>
2025-08-27 20:14:23 -0700

    Time: 1 ms (99.70%) | Memory: 42.1 MB (16.62%) - LeetSync

--- 756-pour-water/pour-water.java
+++ 756-pour-water/pour-water.java
@@ -1,43 +1,39 @@
 class Solution {
     public int[] pourWater(int[] heights, int volume, int k) {
-        if (volume == 0)
-            return heights;
+        if (volume == 0) return heights;
 
-        for (; volume > 0; volume--) {
-            var bestOnLeft = flowLeft(heights, k);
-
-            if (bestOnLeft != k)
-                heights[bestOnLeft]++;
-            else
-                heights[flowRight(heights, k)]++;
+        for (;volume > 0; volume--) {
+        
+            int indexOfWater = findLeftIndex(heights, k);
+            if (indexOfWater != k) {
+                heights[indexOfWater]++;
+            }
+            else {
+                heights[findRightIndex(heights, k)]++;
+            }
         }
-
         return heights;
     }
 
-    private int flowLeft(int[] heights, int k) {
-        var lowest = k;
-
-        for (var i = k - 1; i >= 0; i--) {
-            if (heights[i] > heights[i + 1])
-                break; // uphill, stop
-            if (heights[i] < heights[lowest])
-                lowest = i; // found lower
+    private int findLeftIndex(int[] heights, int k) {
+        int initial = k;
+        for (int i = k - 1; i >= 0; i--) {
+            if (heights[i + 1] < heights[i])
+                break;
+            if (heights[i] < heights[initial])
+                initial = i;
         }
-
-        return lowest;
+        return initial;
     }
 
-    private int flowRight(int[] heights, int k) {
-        var lowest = k;
-
-        for (var i = k + 1; i < heights.length; i++) {
-            if (heights[i] > heights[i - 1])
-                break; // uphill, stop
-            if (heights[i] < heights[lowest])
-                lowest = i;
+    private int findRightIndex(int[] heights, int k) {
+        int initial = k;
+        for (int i = k + 1; i < heights.length; i++) {
+            if (heights[i - 1] < heights[i])
+                break;
+            if (heights[i] < heights[initial])
+                initial = i;
         }
-
-        return lowest;
+        return initial;
     }
 }
