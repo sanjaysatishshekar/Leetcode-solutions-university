ec60544621828f2bb6828665ce65dfb955226f43
Sanjay S S <sanjay.india96@gmail.com>
2025-08-30 12:11:11 -0700

    Time: 39 ms (36.76%) | Memory: 45.6 MB (60.44%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,17 +1,18 @@
 class Solution {
     public String removeDuplicates(String s, int k) {
         StringBuilder sb = new StringBuilder(s);
-        Stack<Integer> counts = new Stack<>();
-        for (int i = 0; i < sb.length(); ++i) {
-            if (i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
-                counts.push(1);
-            } else {
-                int incremented = counts.pop() + 1;
-                if (incremented == k) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < sb.length(); i++) {
+            if (i == 0 || sb.charAt(i) != sb.charAt(i - 1))
+                stack.push(1);
+            else {
+                int increment = stack.pop() + 1;
+                if (increment == k) {
                     sb.delete(i - k + 1, i + 1);
                     i = i - k;
-                } else {
-                    counts.push(incremented);
+                }
+                else {
+                    stack.push(increment);
                 }
             }
         }
