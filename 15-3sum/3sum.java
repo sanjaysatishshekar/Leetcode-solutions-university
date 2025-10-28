2a3e85c5dbb2a2236934ea1301211db35a927e5a
Sanjay S S <sanjay.india96@gmail.com>
2025-10-27 22:03:05 -0700

    Time: 21 ms (99.84%) | Memory: 51.5 MB (56.17%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,31 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
         int n = nums.length;
-        Arrays.sort(nums);
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
+        for (int i = 0; i < n && nums[i] <= 0; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
-
-            int j = i + 1;
-            int k = n - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    while (j < k && nums[j] == nums[j + 1])
-                        j++;
-                    while (j < k && nums[k] == nums[k - 1])
-                        k--;
-                    j++;
-                    k--;
-                } else if (sum < 0)
-                    j++;
-                else
-                    k--;
             }
-
+            twoSum(nums, result, i + 1, n - 1, 0 - nums[i]);
         }
         return result;
+    }
+
+    private void twoSum(int[] nums, List<List<Integer>> result, int left, int right, int target) {
+        while (left < right) {
+            if (nums[left] + nums[right] > target)
+                right--;
+            else if (nums[left] + nums[right] < target)
+                left++;
+            else {
+                result.add(Arrays.asList(-target, nums[left], nums[right]));
+                left++;
+                while (left < right && nums[left] == nums[left - 1]) {
+                    left++;
+                }
+            }
+        }
     }
 }
