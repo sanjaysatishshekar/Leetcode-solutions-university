0926643ef1b6871abdaad0112fd74a40922d3149
Sanjay S S <sanjay.india96@gmail.com>
2025-12-02 20:10:14 -0800

    Time: 2 ms (98.72%) | Memory: 44.2 MB (71.67%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] table = new int[26];
+        int[] table = new int[256];
         for (char c: s.toCharArray()) {
             table[c - 'a']++;
         }
         for (char c: t.toCharArray()) {
             table[c - 'a']--;
         }
-        for (int i: table)
-            if (i != 0)
-                return false;
+        for (int i: table) {
+            if (i != 0) return false;
+        }
         return true;
     }
 }
