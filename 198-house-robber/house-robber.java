43019f76b311b0308dd7dd4b3c3c89af87d5beec
Sanjay S S <sanjay.india96@gmail.com>
2026-01-12 09:32:35 -0800

    Time: 0 ms (100.00%) | Memory: 42.9 MB (21.57%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,15 +1,32 @@
+// class Solution {
+    
+    // public int rob(int[] nums) {
+    //     if (nums == null || nums.length == 0)
+    //         return 0;
+    //     if (nums.length == 1)
+    //         return nums[0];
+    //     int n = nums.length;
+    //     int[] dp = new int[n];
+    //     dp[0] = nums[0];
+    //     if (n > 1)
+    //         dp[1] = Math.max(nums[1], nums[0]);
+    //     for (int i = 2; i < n; i++) {
+    //         dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+    //     }
+    //     return Math.max(dp[n - 1], dp[n - 2]);
+    // }
+// }
 class Solution {
     public int rob(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
-        if (nums.length == 1) return nums[0];
         int n = nums.length;
-        int[] dp = new int[n];
-        dp[0] = nums[0];
-        if (n > 1)
-            dp[1] = Math.max(nums[1], nums[0]);
-        for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
-        }
-        return Math.max(dp[n - 1], dp[n - 2]);
+        int[] memo = new int[n];
+        Arrays.fill(memo, -1);
+        return foo(nums, n - 1, memo);
+    }
+
+    private int foo(int[] nums, int i, int[] memo) {
+        if (i < 0) return 0;
+        if (memo[i] != -1) return memo[i];
+        return memo[i] = Math.max(nums[i] + foo(nums, i - 2, memo), foo(nums, i - 1, memo));
     }
 }
