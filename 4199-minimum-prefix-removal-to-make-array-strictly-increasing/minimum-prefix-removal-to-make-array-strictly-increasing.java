bb973ebb99248f46088ad081aea43833d3b39bb7
Sanjay S S <sanjay.india96@gmail.com>
2026-02-08 17:25:46 -0800

    Time: 1 ms (99.42%) | Memory: 120.2 MB (58.23%) - LeetSync

--- 4199-minimum-prefix-removal-to-make-array-strictly-increasing/minimum-prefix-removal-to-make-array-strictly-increasing.java
+++ 4199-minimum-prefix-removal-to-make-array-strictly-increasing/minimum-prefix-removal-to-make-array-strictly-increasing.java
@@ -1,18 +1,22 @@
 class Solution {
     public int minimumPrefixLength(int[] nums) {
         int result = 0;
-        Stack<Integer> stack = new Stack<>();
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (stack.isEmpty()) {
-                stack.push(nums[i]);
+        int top = -1;
+        int n = nums.length;
+        int prev = nums[n - 1];
+        for (int i = n - 1; i >= 0; i--) {
+            if (top == -1) {
+                prev = nums[i];
+                top = i;
                 continue;
             }
             else {
-                if (stack.peek() <= nums[i]) {
+                if (nums[top] <= nums[i]) {
                     return i + 1;
                 }
                 else {
-                    stack.push(nums[i]);
+                    top--;
+                    prev = nums[i];
                 }
             }
         }
