151dc5e958b49ff0bbb650dfd08178ca52d6756e
Sanjay S S <sanjay.india96@gmail.com>
2026-02-16 00:02:21 -0800

    Time: 4 ms (98.22%) | Memory: 48.1 MB (33.33%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,60 +1,21 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int l = 0;
+        int r = arr.length - 1;
+
+        while(r - l + 1 > k){
+            if (Math.abs(x - arr[l]) <= Math.abs(x - arr[r])) {
+                r--;
+            } else {
+                l++;
+            }
+        }
         List<Integer> result = new ArrayList<>();
 
-        if (arr.length == k) {
-            for (int i = 0; i < k; i++) result.add(arr[i]);
-            return result;
-        }
-        
-        int left = 0, right = arr.length;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] >= x)
-                right = mid;
-            else
-                left = mid + 1;
-        }
-
-        left -= 1;
-        right = left + 1;
-        while (right - left - 1 < k) {
-            if (left == -1) {
-                right += 1;
-                continue;
-            }
-            if (right == arr.length || Math.abs(arr[left] - x) <= Math.abs(arr[right] - x)) {
-                left -= 1;
-            }
-            else {
-                right += 1;
-            }
-        }
-        for (int i = left + 1; i < right; i++) {
+        for(int i = l; i <= r; i++){
             result.add(arr[i]);
         }
+
         return result;
-
-
-        // List<Integer> result = new ArrayList<>();
-        // Map<Integer, List<Integer>> map = new HashMap<>();
-        // for (int num : arr) {
-        //     int key = Math.abs(num - x);
-        //     map.putIfAbsent(key, new ArrayList<>());
-        //     map.get(key).add(num);
-        // }
-
-        // while (k != 0) {
-        //     for (int i = 0; i < arr.length; i++) {
-        //         if (map.containsKey(i)) {
-        //             for (int num : map.get(i)) {
-        //                 result.add(num);
-        //                 k--;
-        //             }
-        //         }
-        //     }
-        // }
-        // return result;
-
     }
 }
