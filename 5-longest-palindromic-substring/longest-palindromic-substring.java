8cee36f6bf5d446b07af48cd5fc14be0878557bb
Sanjay S S <sanjay.india96@gmail.com>
2026-03-02 14:25:00 -0800

    Time: 18 ms (48.00%) | Memory: 43.8 MB (51.64%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,37 +1,33 @@
 class Solution {
     public String longestPalindrome(String s) {
         int n = s.length();
-        int[] result = new int[2];
+        int[] result = new int[]{0, 0};  
         for (int i = 0; i < n; i++) {
-            int start = i;
-            int end = i;
-            int odd = expand(s, start, end);
-            if (odd > result[1] - result[0] + 1) {
-                int dist = odd / 2;
-                result[0] = i - dist;
-                result[1] = i + dist;
+            int start = i, end = i;
+            int len = 0;
+            while (start > -1 && end < n && s.charAt(start) == s.charAt(end)) {
+                len = Math.max(len, end - start + 1);
+                start--; end++;
+            }
+            if (len > result[1] - result[0] + 1) {
+                int diff = len / 2;
+                result[0] = i - diff;
+                result[1] = i + diff;
             }
             start = i;
             end = i + 1;
-            int even = expand(s, start, end);
-            if (even > result[1] - result[0] + 1) {
-                int dist = (even / 2) - 1;
-                result[0] = i - dist;
-                result[1] = i + 1 + dist;
+            len = 0;
+            int even = 0;
+            while (start > -1 && end < n && s.charAt(start) == s.charAt(end)) {
+                len = Math.max(len, end - start + 1);
+                start--; end++;
+            }
+            if (len > result[1] - result[0] + 1) {
+                int diff = len / 2 - 1;
+                result[0] = i - diff;
+                result[1] = i + diff + 1;
             }
         }
         return s.substring(result[0], result[1] + 1);
-    }
-
-    private int expand(String s, int i, int j) {
-        int result = 0;
-        while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
-            if ((j - i + 1) > result) {
-                result = j - i + 1;
-            }
-            i--;
-            j++;
-        }
-        return result;
     }
 }
