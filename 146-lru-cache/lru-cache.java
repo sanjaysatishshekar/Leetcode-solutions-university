eaee6ea5d6f6b68d37f6a201e93a7f663349aa97
Sanjay S S <sanjay.india96@gmail.com>
2026-03-02 14:12:45 -0800

    Time: 48 ms (44.96%) | Memory: 133.8 MB (15.45%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,30 +1,26 @@
 class Node {
-    int key;
-    int val;
-    Node prev;
-    Node next;
-
-    public Node(int key, int val) {
-        this.key = key;
-        this.val = val;
+    int key, val;
+    Node prev, next;
+    Node(int k, int v) {
+        key = k;
+        val = v;
     }
 }
 
 class LRUCache {
-    Map<Integer, Node> dict;
-    Node head;
-    Node tail;
+    Node head, tail;
+    Map<Integer, Node> table;
     int capacity;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        head = new Node(-1, -1);
-        tail = new Node(-1, -1);
+        head = new Node(-1, -1);    
+        tail = new Node(-1, -1);    
         head.next = tail;
         tail.prev = head;
-        dict = new HashMap<>();
+        table = new HashMap<>();
     }
-    
+
     private void add(Node node) {
         Node previousNode = tail.prev;
         previousNode.next = node;
@@ -37,29 +33,29 @@ class LRUCache {
         node.next.prev = node.prev;
         node.prev.next = node.next;
     }
-
+    
     public int get(int key) {
-        if (!dict.containsKey(key)) {
+        if (!table.containsKey(key)) {
             return -1;
         }
-        Node node = dict.get(key);
+        Node node = table.get(key);
         remove(node);
         add(node);
         return node.val;
     }
     
     public void put(int key, int value) {
-        if (dict.containsKey(key)) {
-            Node oldNode = dict.get(key);
-            remove(oldNode);
+        if (table.containsKey(key)) {
+            Node nodeToRemove = table.get(key);
+            remove(nodeToRemove);
         }
         Node newNode = new Node(key, value);
-        dict.put(key, newNode);
         add(newNode);
-        if (dict.size() > capacity) {
-            Node nodeToDel = head.next; 
-            remove(nodeToDel); 
-            dict.remove(nodeToDel.key); 
+        table.put(key, newNode);
+        if (table.size() > this.capacity) {
+            Node nodeToDelete = head.next;
+            remove(nodeToDelete);
+            table.remove(nodeToDelete.key);
         }
     }
 }
