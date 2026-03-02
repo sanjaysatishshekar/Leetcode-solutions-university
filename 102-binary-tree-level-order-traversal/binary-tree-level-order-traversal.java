c884e1574db245b56d3f9c23b8a504ddd6e1719e
Sanjay S S <sanjay.india96@gmail.com>
2026-03-02 12:26:17 -0800

    Time: 1 ms (95.27%) | Memory: 47 MB (39.87%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -16,21 +16,36 @@
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
-        if (root != null) 
-            queue.offer(root);
-        List<Integer> level = new ArrayList<>();
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                level.add(node.val);
-                if (node.left != null) queue.offer(node.left);
-                if (node.right != null) queue.offer(node.right);
-            }
-            result.add(new ArrayList<>(level));
-            level.clear();
-        }
+        if (root == null) return result;
+        dfs(root, 0, result);
         return result;
     }
+
+    private void dfs(TreeNode root, int level, List<List<Integer>> result) {
+        if (result.size() == level) result.add(new ArrayList<Integer>());
+        result.get(level).add(root.val);
+        if (root.left != null) dfs(root.left, level + 1, result);
+        if (root.right != null) dfs(root.right, level + 1, result);
+    }
+
+    // BFS
+    // public List<List<Integer>> levelOrder(TreeNode root) {
+    //     List<List<Integer>> result = new ArrayList<>();
+    //     Queue<TreeNode> queue = new LinkedList<>();
+    //     if (root != null) 
+    //         queue.offer(root);
+    //     List<Integer> level = new ArrayList<>();
+    //     while (!queue.isEmpty()) {
+    //         int size = queue.size();
+    //         for (int i = 0; i < size; i++) {
+    //             TreeNode node = queue.poll();
+    //             level.add(node.val);
+    //             if (node.left != null) queue.offer(node.left);
+    //             if (node.right != null) queue.offer(node.right);
+    //         }
+    //         result.add(new ArrayList<>(level));
+    //         level.clear();
+    //     }
+    //     return result;
+    // }
 }
