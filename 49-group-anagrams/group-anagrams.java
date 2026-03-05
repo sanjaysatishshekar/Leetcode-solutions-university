2c5122f7c15bcc0c6b83fb9c56fb35c852529478
Sanjay S S <sanjay.india96@gmail.com>
2026-03-04 16:03:15 -0800

    Time: 14 ms (30.53%) | Memory: 49.9 MB (48.78%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,15 +1,26 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
         Map<String, List<String>> table = new HashMap<>();
         for (String s: strs) {
-            char[] word = s.toCharArray();
-            Arrays.sort(word);
-            String sortedWord = String.valueOf(word);
-            if (!table.containsKey(sortedWord)) {
-                table.put(sortedWord, new ArrayList<>());
+            String key = getFrequency(s);
+            if (!table.containsKey(key)) {
+                table.put(key, new ArrayList<>());
             }
-            table.get(sortedWord).add(s);
+            table.get(key).add(s);
         }
         return new ArrayList<>(table.values());
+    }
+
+    String getFrequency(String s) {
+        int[] table = new int[26];
+        for (char c: s.toCharArray()) {
+            table[c - 'a']++;
+        }
+        StringBuilder result = new StringBuilder();
+        for (int i: table) {
+            result.append(i).append('#');
+        }
+        return result.toString();
     }
 }
