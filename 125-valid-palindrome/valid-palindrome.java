68629007e6e3f0a15ea9a6d98d7b8f0c54e4f40e
Sanjay S S <sanjay.india96@gmail.com>
2026-03-11 00:21:42 -0700

    Time: 3 ms (61.41%) | Memory: 44.9 MB (47.16%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,16 +1,17 @@
 class Solution {
     public boolean isPalindrome(String s) {
         StringBuilder sb = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9')
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))
                 sb.append(Character.toLowerCase(c));
         }
-        int i = 0, j = sb.length() - 1;
-        while (i < j) {
-            if (sb.charAt(i) != sb.charAt(j))
+        int start = 0, end = sb.length() - 1;
+        while (start < end) {
+            if (sb.charAt(start) != sb.charAt(end))
                 return false;
-            i++;
-            j--;
+            start++;
+            end--;
         }
         return true;
     }
