123785c4167ed8e183336905bf280bf18eb2932c
Sanjay S S <sanjay.india96@gmail.com>
2026-05-13 14:42:15 -0700

    Time: 15 ms (61.37%) | Memory: 43.6 MB (59.95%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,33 +1,33 @@
 class Solution {
     public String longestPalindrome(String s) {
         int n = s.length();
-        int[] result = new int[]{0, 0};  
+        int l = 0;
+        int[] result = new int[2];
         for (int i = 0; i < n; i++) {
-            int start = i, end = i;
-            int len = 0;
-            while (start > -1 && end < n && s.charAt(start) == s.charAt(end)) {
-                len = Math.max(len, end - start + 1);
-                start--; end++;
-            }
-            if (len > result[1] - result[0] + 1) {
-                int diff = len / 2;
+            int odd = expand(s, i, i);
+            if (odd > result[1] - result[0] + 1) {
+                int diff = odd / 2;
                 result[0] = i - diff;
                 result[1] = i + diff;
             }
-            start = i;
-            end = i + 1;
-            len = 0;
-            int even = 0;
-            while (start > -1 && end < n && s.charAt(start) == s.charAt(end)) {
-                len = Math.max(len, end - start + 1);
-                start--; end++;
-            }
-            if (len > result[1] - result[0] + 1) {
-                int diff = len / 2 - 1;
+            int even = expand(s, i, i + 1);
+            if (even > result[1] - result[0] + 1) {
+                int diff = even / 2 - 1;
                 result[0] = i - diff;
                 result[1] = i + diff + 1;
             }
         }
         return s.substring(result[0], result[1] + 1);
+    }
+
+    private int expand(String s, int start, int end) {
+        int n = s.length();
+        int result = 0;
+        while (start > -1 && end < n && s.charAt(start) == s.charAt(end)) {
+            result = Math.max(result, end - start + 1);
+            start--;
+            end++;
+        }
+        return result;
     }
 }
