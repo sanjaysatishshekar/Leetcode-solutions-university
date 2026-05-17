506eb5648813608d4d8d43cdfb2cee527632aedf
Sanjay S S <sanjay.india96@gmail.com>
2026-05-17 13:56:47 -0700

    Time: 24 ms (99.90%) | Memory: 59 MB (84.79%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -4,26 +4,27 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         int n = nums.length;
         for (int i = 0; i < n && nums[i] <= 0; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            twoSum(nums, result, i + 1, n - 1, 0 - nums[i]);
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            twoSum(nums, result, i + 1, n - 1, -nums[i]);
         }
         return result;
     }
 
-    private void twoSum(int[] nums, List<List<Integer>> result, int left, int right, int target) {
-        while (left < right) {
-            if (nums[left] + nums[right] > target)
-                right--;
-            else if (nums[left] + nums[right] < target)
-                left++;
+    private void twoSum(int[] nums, List<List<Integer>> result, int i, int j, int target) {
+        while (i < j) {
+            int sum = nums[i] + nums[j];
+            if (sum < target) {
+                i++;
+            }
+            else if (sum > target) {
+                j--;
+            }
             else {
-                result.add(Arrays.asList(-target, nums[left], nums[right]));
-                left++;
-                while (left < right && nums[left] == nums[left - 1]) {
-                    left++;
-                }
+                // System.out.println(nums[i] + " " + nums[j] + " " + (-target));
+                result.add(Arrays.asList(-target, nums[i], nums[j]));
+                i++;
+                while (i < j && nums[i] == nums[i - 1])
+                    i++;
             }
         }
     }
