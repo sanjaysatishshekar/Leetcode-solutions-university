d92a03f744601a0ffd0e50c98e27707171486510
Sanjay S S <sanjay.india96@gmail.com>
2026-05-17 14:28:51 -0700

    Time: 8 ms (91.13%) | Memory: 49.3 MB (12.97%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -2,12 +2,12 @@ class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         List<int[]> merged = new ArrayList<>();
-        int[] temp = intervals[0];
+        int[] temp =  intervals[0];
         for (int[] interval: intervals) {
-            if ((interval[0] >= temp[0] && interval[0] <= temp[1])
-            || interval[1] >= temp[0] && interval[1] <= temp[1]) {
-                temp[0] = Math.min(temp[0], interval[0]);
-                temp[1] = Math.max(temp[1], interval[1]);
+            if ((temp[0] <= interval[0] &&  interval[0] <= temp[1])
+                || (temp[0] <= interval[1] && interval[1] <= temp[1]))  {
+                    temp[0] = Math.min(temp[0], interval[0]);
+                    temp[1] = Math.max(temp[1], interval[1]);
             }
             else {
                 merged.add(temp);
@@ -15,10 +15,10 @@ class Solution {
             }
         }
         merged.add(temp);
-        int[][] result = new int[merged.size()][2];
+        int[][] answer = new int[merged.size()][];
         for (int i = 0; i < merged.size(); i++) {
-            result[i] = merged.get(i);
+            answer[i] = merged.get(i);
         }
-        return result;
+        return answer;
     }
 }
