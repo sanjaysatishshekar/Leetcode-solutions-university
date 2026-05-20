3049b8b7b33b197dc08971c7080840fc694e54b5
Sanjay S S <sanjay.india96@gmail.com>
2026-05-19 23:44:47 -0700

    Time: 3 ms (59.88%) | Memory: 44.4 MB (64.97%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,16 +1,38 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder();
-        for (char c: s.toCharArray()) {
-            if (Character.isAlphabetic(c) || Character.isDigit(c)) {
-                sb.append(Character.toLowerCase(c));
+        // StringBuilder sb = new StringBuilder();
+        // for (char c: s.toCharArray()) {
+        //     if (Character.isAlphabetic(c) || Character.isDigit(c)) {
+        //         sb.append(Character.toLowerCase(c));
+        //     }
+        // }
+        // int i = 0, j = sb.length() - 1;
+        // while (i < j) {
+        //     if (sb.charAt(i) != sb.charAt(j)) return false;
+        //     i++; j--;
+        // }
+        // return true;
+
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (!isAlphaNumeric(s.charAt(l))) {
+                l++;
+                continue;
             }
-        }
-        int i = 0, j = sb.length() - 1;
-        while (i < j) {
-            if (sb.charAt(i) != sb.charAt(j)) return false;
-            i++; j--;
+            if (!isAlphaNumeric(s.charAt(r))) {
+                r--;
+                continue;
+            }
+
+            char a = Character.toLowerCase(s.charAt(l));
+            char b = Character.toLowerCase(s.charAt(r));
+            if (a != b) return false;
+            l++; r--;
         }
         return true;
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return Character.isAlphabetic(c) || Character.isDigit(c);
     }
 }
