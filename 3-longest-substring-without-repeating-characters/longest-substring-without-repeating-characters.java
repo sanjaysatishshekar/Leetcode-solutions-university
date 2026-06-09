13768ae6b7556c61acb33af3c64324677179cc05
Sanjay S S <sanjay.india96@gmail.com>
2026-06-09 10:57:44 -0700

    Time: 2 ms (97.64%) | Memory: 44.7 MB (92.20%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -4,13 +4,18 @@ class Solution {
         int n = s.length();
         int l = 0;
         int[] table = new int[256];
+        Arrays.fill(table, -1);
         int longest = 0;
         for (int r = 0; r < n; r++) {
             char c = s.charAt(r);
-            table[c]++;
-            while (l <= r && table[c] > 1) {
-                table[s.charAt(l++)]--;
+            if (table[c] >= l) {
+                l = table[c] + 1;
             }
+            table[c] = r;
+            // table[c]++;
+            // while (l <= r && table[c] > 1) {
+            //     table[s.charAt(l++)]--;
+            // }
             longest = Math.max(longest, r - l + 1);
         }
         return longest;
