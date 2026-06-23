94df216d2477088b5014aa320d9eef970e5f12a2
Sanjay S S <sanjay.india96@gmail.com>
2026-06-23 13:53:17 -0700

    Time: 5 ms (100.00%) | Memory: 47.9 MB (41.32%) - LeetSync

--- 4340-maximum-manhattan-distance-after-all-moves/maximum-manhattan-distance-after-all-moves.java
+++ 4340-maximum-manhattan-distance-after-all-moves/maximum-manhattan-distance-after-all-moves.java
@@ -1,29 +1,34 @@
 class Solution {
     public int maxDistance(String moves) {
-        int underscore = 0;
-        int[] current = {0, 0};
-        int[][] table = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        int[] table = new int[128];
         for (char move: moves.toCharArray()) {
-            if (move == 'L') {
-                current[0] += table[0][0];
-                current[1] += table[0][1];
-            }
-            else if (move == 'U') {
-                current[0] += table[1][0];
-                current[1] += table[1][1];
-            }
-            else if (move == 'R') {
-                current[0] += table[2][0];
-                current[1] += table[2][1];
-            }
-            else if (move == 'D') {
-                current[0] += table[3][0];
-                current[1] += table[3][1];
-            }
-            else {
-                underscore++;
-            }
+            table[move]++;
         }
-        return Math.abs(current[0]) + Math.abs(current[1]) + underscore;
+        return Math.abs(table['L'] - table['R']) + Math.abs(table['U'] - table['D']) + table['_'];
+        // int underscore = 0;
+        // int[] current = {0, 0};
+        // int[][] table = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        // for (char move: moves.toCharArray()) {
+        //     if (move == 'L') {
+        //         current[0] += table[0][0];
+        //         current[1] += table[0][1];
+        //     }
+        //     else if (move == 'U') {
+        //         current[0] += table[1][0];
+        //         current[1] += table[1][1];
+        //     }
+        //     else if (move == 'R') {
+        //         current[0] += table[2][0];
+        //         current[1] += table[2][1];
+        //     }
+        //     else if (move == 'D') {
+        //         current[0] += table[3][0];
+        //         current[1] += table[3][1];
+        //     }
+        //     else {
+        //         underscore++;
+        //     }
+        // }
+        // return Math.abs(current[0]) + Math.abs(current[1]) + underscore;
     }
 }
