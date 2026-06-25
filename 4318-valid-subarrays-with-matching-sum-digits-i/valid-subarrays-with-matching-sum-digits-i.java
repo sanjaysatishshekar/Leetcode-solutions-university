246b9410efe02e9a3ea8ce145f44bf5041d3f647
Sanjay S S <sanjay.india96@gmail.com>
2026-06-25 02:15:37 -0700

    Time: 466 ms (61.06%) | Memory: 47.1 MB (54.34%) - LeetSync

--- 4318-valid-subarrays-with-matching-sum-digits-i/valid-subarrays-with-matching-sum-digits-i.java
+++ 4318-valid-subarrays-with-matching-sum-digits-i/valid-subarrays-with-matching-sum-digits-i.java
@@ -2,30 +2,29 @@ class Solution {
     public int countValidSubarrays(int[] nums, int x) {
         int n = nums.length;
         int count = 0;
-        long[] prefix = new long[n];
-        prefix[0] = nums[0];
-        if (check(nums[0], x)) count++; 
-        
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1] + nums[i];
-            if (check(prefix[i], x))
-                count++;
-            if (check(nums[i], x))
-                count++;
-        }
-        
-        for (int i = 1; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (check(prefix[j] - prefix[i - 1], x))
-                    count++;
+        for (int i = 0; i < n; i++) {
+            long currentSum = 0;
+            for (int j = i; j < n; j++) {
+                currentSum += nums[j];
+                count = count + check(currentSum, x);
             }
         }
         return count;
     }
-    
-    private boolean check(long sum, int x) {
-        String num = String.valueOf(sum);
-        return (num.charAt(0) - '0') == x && (num.charAt(num.length() - 1) - '0') == x;
+
+    private int check(long sum, int x) {
+        int count = 0;
+        if (sum % 10 == x) {
+            count++;
+            long temp = sum;
+            while (temp >= 10) {
+                temp /= 10;
+            }
+            if (temp == x) {
+                count++;
+            }
+        }
+        return count == 2 ? 1: 0;
     }
 }
 
@@ -33,7 +32,6 @@ class Solution {
 // 1 101 102
 
 // 102 101 1
-
 
 // a, b, c, d
 // a, a+b, a+b+c, a+b+c+d
