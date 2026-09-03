ac138e682e1d2e2e9ffb1385588b0bdb5d62ad54
Sanjay S S <sanjay.india96@gmail.com>
2026-09-02 18:18:16 -0700

    Time: 2 ms (89.01%) | Memory: 43.6 MB (98.90%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,56 +1,33 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        Queue<Pair<Integer, Integer>> queue = new ArrayDeque();
-
-        // Step 1). build the initial set of rotten oranges
-        int freshOranges = 0;
-        int ROWS = grid.length, COLS = grid[0].length;
-
-        for (int r = 0; r < ROWS; ++r)
-            for (int c = 0; c < COLS; ++c)
-                if (grid[r][c] == 2)
-                    queue.offer(new Pair(r, c));
-                else if (grid[r][c] == 1)
-                    freshOranges++;
-
-        // Mark the round / level, _i.e_ the ticker of timestamp
-        queue.offer(new Pair(-1, -1));
-
-        // Step 2). start the rotting process via BFS
-        int minutesElapsed = -1;
-        int[][] directions = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
-
-        while (!queue.isEmpty()) {
-            Pair<Integer, Integer> p = queue.poll();
-            int row = p.getKey();
-            int col = p.getValue();
-            if (row == -1) {
-                // We finish one round of processing
-                minutesElapsed++;
-                // to avoid the endless loop
-                if (!queue.isEmpty())
-                    queue.offer(new Pair(-1, -1));
-            } else {
-                // this is a rotten orange
-                // then it would contaminate its neighbors
-                for (int[] d : directions) {
-                    int neighborRow = row + d[0];
-                    int neighborCol = col + d[1];
-                    if (neighborRow >= 0 && neighborRow < ROWS &&
-                            neighborCol >= 0 && neighborCol < COLS) {
-                        if (grid[neighborRow][neighborCol] == 1) {
-                            // this orange would be contaminated
-                            grid[neighborRow][neighborCol] = 2;
-                            freshOranges--;
-                            // this orange would then contaminate other oranges
-                            queue.offer(new Pair(neighborRow, neighborCol));
-                        }
+        int m = grid.length;
+        int n = grid[0].length;
+        int fresh = 0;
+        Queue<int[]> q = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) fresh++;
+                else if (grid[i][j] == 2) q.offer(new int[]{i, j});
+            }
+        }
+        int time = 0;
+        int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        while (fresh > 0 && !q.isEmpty()) {
+            int size = q.size();
+                for (int i = 0; i < size; i++) {
+                int[] node = q.poll();
+                for (int[] d: dirs) {
+                    int r = node[0] + d[0];
+                    int c = node[1] + d[1];
+                    if (r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == 1) {
+                        grid[r][c] = 2;
+                        q.offer(new int[]{r, c});
+                        fresh--;
                     }
                 }
             }
+            time++;
         }
-
-        // return elapsed minutes if no fresh orange left
-        return freshOranges == 0 ? minutesElapsed : -1;
+        return fresh == 0 ? time : -1;
     }
 }
